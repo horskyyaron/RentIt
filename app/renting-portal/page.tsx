80c0ae381e014ccff3b60634a4efe99ba35aed06
Yaron <horsky.yaron@gmail.com>
2023-06-14 15:56:59 +0300

    added uploadthing for adding images for the inventory

--- app/renting-portal/page.tsx
+++ app/renting-portal/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+ 
+// You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
+import "@uploadthing/react/styles.css";
+ 
+import { UploadButton } from "@uploadthing/react";
+ 
+import { OurFileRouter } from "../api/uploadthing/core";
+ 
 export default function RentingPortal() {
-    return (
-        <>
-            <h1>portal</h1>
-        </>
-    )
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <UploadButton<OurFileRouter>
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+          console.log(res[0]["fileUrl"])
+          
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
+    </main>
+  );
 }
