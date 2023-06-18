be4a95b234fb23cbffd363067076ef742846aa4d
Yaron <horsky.yaron@gmail.com>
2023-06-18 12:55:47 +0300

    escaped some commas

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,10 +4,10 @@ export default async function About() {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-8">About Us</h1>
       <p className="text-lg max-w-md text-center">
-        Welcome to our renting web application! We provide a convenient platform for users to rent and offer various items for rent. Whether you're looking to rent out your unused items or in need of renting something for a short period, our application has got you covered.
+        Welcome to our renting web application! We provide a convenient platform for users to rent and offer various items for rent. Whether you&apos;re looking to rent out your unused items or in need of renting something for a short period, our application has got you covered.
       </p>
         <p className="text-lg  max-w-md text-center mt-4">
-          With our user-friendly interface, you can easily browse through a wide range of available items, explore their details, and connect with the owners to arrange the rental. From tools and equipment to household items and electronics, you'll find a diverse selection of items to choose from.
+          With our user-friendly interface, you can easily browse through a wide range of available items, explore their details, and connect with the owners to arrange the rental. From tools and equipment to household items and electronics, you&apos;ll find a diverse selection of items to choose from.
         </p>
         <p className="text-lg  max-w-md text-center mt-4">
           Our application prioritizes user satisfaction and safety. We ensure that all users undergo a verification process to establish trust within our community. Additionally, we provide secure messaging features to facilitate communication and negotiation between renters and item owners.
