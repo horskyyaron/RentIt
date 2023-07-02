41fd404accb7915f4dde82ce78bcf517b1c9cbbf
Yaron <horsky.yaron@gmail.com>
2023-07-02 12:51:10 +0300

    fixed renting portal to new types

--- app/renting-portal/Gallery.tsx
+++ app/renting-portal/Gallery.tsx
@@ -1,41 +1,52 @@
 import prisma from "@/lib/db";
 import RentCard from "./RentCard";
+import { currentUser } from "@clerk/nextjs";
 
 export const revalidate = 60; // revalidate this page every 60 seconds
 
 export default async function Gallery() {
-  const cards = await prisma.rentingQueryCard.findMany({
+  const user = await currentUser().then(async (clerk) => {
+    return prisma.user.findUnique({
+      where: {
+        clerkId: clerk?.id,
+      },
+    });
+  });
+
+  const cards = await prisma.rentCard.findMany({
+    where: {
+      proposerId: user?.clerkId,
+    },
     include: {
       item: {
         include: {
           images: true,
         },
       },
+      rentingDays: true,
     },
   });
 
   if (cards.length == 0) {
     return (
-      <div className="flex flex-col items-center justify-center mt-24">
-        <h1 className="text-4xl font-bold font-ysabeau mb-8">No available requests or offers so far...</h1>
-        <h2 className="text-xl font-ysabeau mt-2">What are you waiting for? go and rent something or ask for an item yourself!</h2>
+      <div className="mt-24 flex flex-col items-center justify-center">
+        <h1 className="mb-8 font-ysabeau text-4xl font-bold">
+          No available requests or offers so far...
+        </h1>
+        <h2 className="mt-2 font-ysabeau text-xl">
+          What are you waiting for? go and rent something or ask for an item
+          yourself!
+        </h2>
       </div>
     );
   } else {
     return (
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {cards.map((card) => (
             <div key={card.id} className="mb-1">
-              <RentCard
-                name={card.item?.name || ""}
-                description={card.item?.description || ""}
-                owner_id={card.proposerId || ""}
-                price={card.item?.rentPerDay || 1}
-                images={card.item?.images}
-                query_type={card.type}
-                published={card.createdAt}
-              />
+              {/* @ts-ignore */}
+              <RentCard card={card} />
             </div>
           ))}
         </div>
--- app/renting-portal/RentCard.tsx
+++ app/renting-portal/RentCard.tsx
@@ -1,10 +1,10 @@
 import BlurImage from "@/app/renting-portal/BlurImage";
 import { clerkClient } from "@clerk/nextjs";
-import { QueryType } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import { CardType } from "@/lib/types";
 dayjs.extend(relativeTime);
 
 type Image = {
@@ -12,47 +12,30 @@ type Image = {
   fileUrl: string;
 };
 
-interface CardProps {
-  name: string;
-  description: string;
-  price: number;
-  owner_id: string;
-  images: Image | any;
-  query_type: QueryType;
-  published: Date;
-}
+export default async function RentCard({ card }: { card: CardType }) {
 
-export default async function RentCard({
-  name,
-  description,
-  price,
-  owner_id,
-  images,
-  query_type,
-  published,
-}: CardProps) {
-  const user = await clerkClient.users.getUser(owner_id);
+  const user = await clerkClient.users.getUser(card.proposerId);
 
   return (
     <div
       className={`${
-        query_type === QueryType.ASK ? "bg-purple-800" : "bg-blue-800"
+        card.type == "OFFER" ? "bg-purple-800" : "bg-blue-800"
       } w-full max-w-sm rounded-lg border border-gray-200 shadow dark:border-gray-700 dark:bg-gray-800`}
     >
       <div className="mb-2 mt-2 flex items-center justify-center">
-        <h1 className="text-center text-white">{name}</h1>
+        <h1 className="text-center text-white">{card.item.name}</h1>
       </div>
       <div className="px-5 text-white">
         <div className="relative aspect-square">
-          <BlurImage img_url={images[0].fileUrl} />
+          <BlurImage img_url={card.item.images[0].fileUrl} />
         </div>
         <div className="my-2">
           <div className="text-sm text-white">
-            <p>{description}</p>
+            <p>{card.item.description}</p>
           </div>
           <div className="flex items-center justify-between">
             <span className="text-lg font-bold dark:text-white">
-              {price} 🪙/per day
+              {card.rentPerDay} 🪙/per day
             </span>
           </div>
         </div>
@@ -63,14 +46,20 @@ export default async function RentCard({
       {/*   <Badge variant="default">Badge</Badge> */}
       {/* </div> */}
       <div className="border-t border-gray-200 dark:border-gray-700" />
-      <Link href={`/user/${owner_id}`}>
+      <Link href={`/user/${card.proposerId}`}>
         <div className="my-2 flex items-center justify-center space-x-1">
-          <Image src={user.imageUrl} alt="" className="rounded-full" width={30} height={30}/>
+          <Image
+            src={user.imageUrl}
+            alt=""
+            className="rounded-full"
+            width={30}
+            height={30}
+          />
           <p>{user.username ? user.username : user.firstName}</p>
         </div>
         <div>
           <p className="text-center">
-            created at: {dayjs(published).fromNow()}
+            created at: {dayjs(card.createdAt).fromNow()}
           </p>
         </div>
       </Link>
